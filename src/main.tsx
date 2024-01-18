7e98fb7bc14876721ed91f9af8e437ede3e9062b
Davi Souza <dnsbr7@gmail.com>
2024-01-17 21:55:32 -0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,29 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 import "./lib/i18n.ts"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { Home } from "./pages/Home/index.tsx"
+import { Projects } from "./pages/Projects/index.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/vite-react-router/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-react-router/",
+        element: <Home />,
+      },
+      {
+        path: "/vite-react-router/contact",
+        element: <Projects />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
